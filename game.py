1076764c8e13d3304bec2a5b8b55115d3a1a25ae
hershedtilak <hershedtilak@gmail.com>
2016-08-28 18:36:03 -0700

    Score Text
    Added score text and reorganized structure of data files

--- game.py
+++ game.py
@@ -11,11 +11,15 @@ class Game:
 	BG_COLOR = pygame.Color(255,255,255)
 	snakes = []
 	controllers = []
+	scores = []
 	fruit = Fruit()
 	running = True
 	surf = pygame.display.set_mode((BOARD_WIDTH*BLOCK_SIZE,BOARD_HEIGHT*BLOCK_SIZE), pygame.HWSURFACE)
+	score_font = None;
 	
 	def __init__(self):
+		pygame.font.init()
+		self.score_font = pygame.font.SysFont("monospace", 12)
 		self.surf.fill(self.BG_COLOR)
 		for i in range (0,NUM_SNAKES):
 			startDir = randint(0,3)
@@ -23,14 +27,18 @@ class Game:
 			newPlayer = Snake(openCell[0],openCell[1],startDir,pygame.Color(20*i,0,50*i),self.BG_COLOR)
 			self.snakes.append(newPlayer)
 			self.controllers.append(Controller(GAMMA))
-			if os.path.isfile('data/valFunc_' + self.controllers[i].getID() + '.npy') and \
-				os.path.isfile('data/transitionMatrix_' + self.controllers[i].getID() + '.npy'):
-				self.controllers[i].loadValFunc('data/valFunc_' + self.controllers[i].getID() + '.npy')
-				self.controllers[i].loadTransitionMatrix('data/transitionMatrix_' + self.controllers[i].getID() + '.npy')
+			self.scores.append(0)
+			if not os.path.exists('data/P' + str(i)):
+				os.makedirs('data/P' + str(i))
+			if os.path.isfile('data/P' + str(i) + '/valFunc_' + self.controllers[i].getID() + '.npy') and \
+				os.path.isfile('data/P' + str(i) + '/transitionMatrix_' + self.controllers[i].getID() + '.npy'):
+				self.controllers[i].loadValFunc('data/P' + str(i) + '/valFunc_' + self.controllers[i].getID() + '.npy')
+				self.controllers[i].loadTransitionMatrix('data/P' + str(i) + '/transitionMatrix_' + self.controllers[i].getID() + '.npy')
 			newPlayer.drawSpawn(self.surf,BLOCK_SIZE)
 		openCell = self.getOpenCell()
 		self.fruit.moveFruit(openCell[0],openCell[1])
 		self.fruit.drawFruit(self.surf,BLOCK_SIZE)
+		self.drawScores()
 
 	def getOpenCell(self):
 		x = randint(0,BOARD_WIDTH-1)
@@ -83,6 +91,8 @@ class Game:
 		# update board state
 		for i in range (0,NUM_SNAKES):
 			if self.snakes[i].isDead:
+				self.scores[i] = 0
+				self.drawScores()
 				self.snakes[i].removeFromBoard()
 				self.snakes[i].drawDead(self.surf, BLOCK_SIZE)
 				startDir = randint(0,3)
@@ -90,6 +100,8 @@ class Game:
 				self.snakes[i].reset(openCell[0], openCell[1], startDir)
 				self.snakes[i].drawSpawn(self.surf, BLOCK_SIZE)
 			elif self.snakes[i].ateFruit:
+				self.scores[i] = self.scores[i] + 1
+				self.drawScores()
 				self.snakes[i].updateFruit()
 				self.snakes[i].drawFruit(self.surf, BLOCK_SIZE)
 				openCell = self.getOpenCell()
@@ -97,7 +109,15 @@ class Game:
 				self.fruit.drawFruit(self.surf,BLOCK_SIZE)
 			else:
 				self.snakes[i].drawNormal(self.surf, BLOCK_SIZE)
-			
+		
+	def drawScores(self):
+		score_string = ""
+		for i in range(0, NUM_SNAKES):
+			score_string = score_string + "P" + str(i+1) + ":" + str(self.scores[i]) + "     "
+		scoretext = self.score_font.render(score_string, 1, (0,0,0))
+		self.surf.fill(self.BG_COLOR, scoretext.get_rect())
+		self.surf.blit(scoretext, scoretext.get_rect())
+		
 	def execute(self):
 		while self.running == True:
 			pygame.event.pump()
@@ -108,8 +128,8 @@ class Game:
 			pygame.display.flip()
 			time.sleep (UPDATE_INTERVAL / 1000.0);
 		for i in range(0,NUM_SNAKES):
-			self.controllers[i].saveValFunc('data/valFunc_' + self.controllers[i].getID() + '.npy')
-			self.controllers[i].saveTransitionMatrix('data/transitionMatrix_' + self.controllers[i].getID() + '.npy')
+			self.controllers[i].saveValFunc('data/P' + str(i) + '/valFunc_' + self.controllers[i].getID() + '.npy')
+			self.controllers[i].saveTransitionMatrix('data/P' + str(i) + '/transitionMatrix_' + self.controllers[i].getID() + '.npy')
 		pygame.quit()
 			
 game = Game()
